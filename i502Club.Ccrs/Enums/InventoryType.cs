0b396c423f406db2f215e714137a189841c593fd
DNNMonster <coryg@storeoborealis.com>
2022-04-28 07:47:18 -0700

    Add InventoryType enum superset

---
+++ i502Club.Ccrs/Enums/InventoryType.cs
@@ -0,0 +1,43 @@
+﻿namespace i502Club.Ccrs.Enums
+{
+    /// <summary>
+    /// Experimental Superset Enum which include all InventoryCategory InventoryTypes.
+    /// Standardized enum key names for Waste and Marijuana Mix values have 
+    /// been altered with InventoryCategory suffixes to allow for a single Enum.
+    /// </summary>
+    public enum InventoryType
+    {
+        Seed = 1,
+        Plant,
+        WetFlower,
+        WetOtherMaterial,
+        FlowerUnlotted,
+        FlowerLot,
+        OtherMaterialUnlotted,
+        OtherMaterialLot,
+        WasteHarvestMaterial,
+        MarijuanaMixHarvestMaterial,
+        MarijuanaMixIntermediateProduct,
+        ConcentrateforInhalation,
+        NonSolventbasedConcentrate,
+        HydrocarbonConcentrate,
+        CO2Concentrate,
+        EthanolConcentrate,
+        FoodGradeSolventConcentrate,
+        InfusedCookingMedium,
+        CBD,
+        WasteIntermediateProduct,
+        Capsule,
+        SolidEdible,
+        Tincture,
+        LiquidEdible,
+        Transdermal,
+        TopicalOintment,
+        MarijuanaMixPackaged,
+        MarijuanaMixInfused,
+        Suppository,
+        UsableMarijuana,
+        SampleJar,
+        WasteEndProduct
+    }
+}
