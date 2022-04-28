27f8894b4b81db301a95ab8c89b6e14da8fe936c
DNNMonster <coryg@storeoborealis.com>
2022-04-28 08:51:37 -0700

    Implement product test enum usage

--- i502Club.Ccrs.Tests/ProductTests.cs
+++ i502Club.Ccrs.Tests/ProductTests.cs
@@ -37,8 +37,7 @@ namespace i502Club.Ccrs.Tests
                 {
                     Name = "Product" + i,
                     InventoryCategory = Enums.InventoryCategory.PropagationMaterial,
-                    //InventoryType = Enum.GetName(typeof(Enums.InventoryType),(int)Enums.InventoryType.Seed),
-                    InventoryType = "Seed",
+                    InventoryType = Enum.GetName(typeof(Enums.InventoryType),(int)Enums.InventoryType.Seed),
                     CreatedDate = DateTime.Parse("04/20/2022"),
                     CreatedBy = user.FirstName + " " + user.LastName
                 };
