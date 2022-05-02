b363ae686fccee2c41b8a346f18bb3ef60fa1df6
DNNMonster <coryg@storeoborealis.com>
2022-05-02 16:10:10 -0700

    Implement Product prop InventoryType

--- i502Club.Ccrs/Models/Product.cs
+++ i502Club.Ccrs/Models/Product.cs
@@ -6,7 +6,7 @@ namespace i502Club.Ccrs.Models
     {
         public string LicenseNumber { get; set; }
         public InventoryCategory InventoryCategory { get; set; }
-        public string InventoryType { get; set; }
+        public InventoryType InventoryType { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal UnitWeightGrams { get; set; }
